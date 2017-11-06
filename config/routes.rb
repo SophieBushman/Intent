2f043c866a2fabc902cf507ccbf21cb77b0cf21d
Veronica Martinez <martinezvea@gmail.com>
2017-11-06 16:54:16 -0500

    home changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :homes
   devise_for :models
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root to: "home#index"
+
+root 'homes#index'
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171106212641) do
+ActiveRecord::Schema.define(version: 20171106213844) do
+
+  create_table "homes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
