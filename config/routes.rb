50fa93e9069c44ef8d30a55ecdd6b8584b3c126a
vpkirby002 <vpkirby002@gmail.com>
2017-11-14 10:23:57 -0500

    calendar updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
+  resources :events
   devise_for :models
-  resources :reflections 
   resources :manifestations
   resources :models
   resources :intentions
   resources :homes
-
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'homes#index'
-root 'reflections#show'
-root 'models#edit'
 
 end
